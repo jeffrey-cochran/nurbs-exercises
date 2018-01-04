16d9420c90938f4f5b62663d3c4b44afd99e2c7e
jeffrey-cochran <koenigcochran@gmail.com>
2018-01-04 13:32:48 -0500

    solve problem 4. allow implicit and parametric plotting

--- chapter_one/problem_4.py
+++ chapter_one/problem_4.py
@@ -3,10 +3,52 @@ Created on Jan 4, 2018
 
 @author: jeffr
 '''
-from utils.visualization import plot_2D_implicit_function
+import matplotlib.pyplot as plt
+from utils.visualization import plot_2D_implicit_function,\
+    plot_2D_parametric_function
 
 
-def original_implicit_curve(y, x):
+#
+# Define the original implicit equation
+def original_implicit_curve(x, y):
     return x**2.0 - 4*x*y + 4*y**2.0 - 4*x - y - 5
 
-plot_2D_implicit_function(original_implicit_curve, num_pts=1000)
+#
+# Define rotated 90deg + translated (-1, -1) implicit equation
+def transformed_implicit_curve(x, y):
+    return y**2.0 + 4*x**2.0 + 4*x*y + 2*y + 13*x - 3
+
+#
+# Define original parametric curve
+def original_parametric_curve(u):
+    return (-1 - u + (2 * u**2.0), (-2 * u) + u**2.0)
+
+#
+# Define the transformed parametric curve
+def transformed_parametric_curve(u):
+    return (-1 + (2 * u) - u**2.0, -2 - u + (2 * u**2.0))
+
+#
+# Plot the original implicit equation
+plot_2D_implicit_function(original_implicit_curve, label='Original Implicit Equation')
+
+#
+# Plot the transformed implicit equation
+plot_2D_implicit_function(transformed_implicit_curve, label='Transformed Implicit Equation')
+
+plt.legend()
+plt.show()
+plt.clf()
+
+#
+# Plot the original parametric equation
+plot_2D_parametric_function(original_parametric_curve, u_range=(-5, 5), label='Original Parametric Equation')
+
+#
+# Plot the original parametric equation
+plot_2D_parametric_function(transformed_parametric_curve, u_range=(-5, 5), label='Transformed Parametric Equation')
+
+plt.xlim(-10, 10)
+plt.ylim(-10, 10)
+plt.legend()
+plt.show()
--- utils/visualization.py
+++ utils/visualization.py
@@ -1,20 +1,48 @@
 from scipy.optimize import root
-from numpy import linspace, zeros
+from numpy import linspace, zeros, meshgrid
 import matplotlib.pyplot as plt
 
-def plot_2D_implicit_function(implicit_function, x_range=(0, 10,), num_pts=100):
+def plot_2D_implicit_function(implicit_function, x_range=(-10, 10,), y_range=(-10, 10), num_pts=100, ax=None,
+                              **kwargs):
     #
+    # This function assumes that the implicit function
+    # is defined in such a way as to be equal to zero.
+    # This can be easily accomplished by subtracting
+    # any nonzero term from the implicit function
+    # definition. 
     x_discretization = linspace(x_range[0], x_range[1], num_pts)
+    y_discretization = linspace(y_range[0], x_range[1], num_pts)
     #
-    y = zeros(x_discretization.shape)
-    for i in range(x_discretization.shape[0]):
-        res = root(implicit_function, x0=[0], args=(x_discretization[i],))
-        y[i] = res.x
-    print(sum(implicit_function(y, x_discretization)))
+    X, Y = meshgrid(x_discretization, y_discretization)
+    Z = implicit_function(X, Y)
     #
-    plt.plot(x_discretization, y)
-    plt.show()
+    # Get contour
+    contours = plt.contour(X, Y, Z, [0])
+    paths = contours.collections[0].get_paths()[0]
+    vertices = paths.vertices
+    x = vertices[:, 0]
+    y = vertices[:, 1]
+    #
+    # Either put it on an axis or
+    # just use current
+    if ax is None:
+        ax = plt.gca()
+    #
+    # Plot
+    ax.plot(x, y, **kwargs)
     
 
-def implicit_parabola(y, x):
-    return x**2.0 - y
+def plot_2D_parametric_function(parametric_function, u_range=(0, 1,), num_pts=100, ax=None,
+                                **kwargs):
+    #
+    # discretize
+    u_discretization = linspace(u_range[0], u_range[1], num_pts)
+    (x, y) = parametric_function(u_discretization)
+    #
+    # Either put it on an axis or
+    # just use current
+    if ax is None:
+        ax = plt.gca()
+    #
+    # Plot
+    ax.plot(x, y, **kwargs)
