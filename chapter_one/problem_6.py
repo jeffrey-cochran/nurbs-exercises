d3db92dc9fa5314de993c0557d1061bc8efa8001
jeffrey-cochran <koenigcochran@gmail.com>
2018-01-05 16:17:29 -0500

    create loop

--- chapter_one/problem_6.py
+++ chapter_one/problem_6.py
@@ -4,10 +4,10 @@ import matplotlib.pyplot as plt
 
 
 points = (
-    asarray([-1, -1]),
-    asarray([1, 1]),
-    asarray([-1.5, -1.5]),
-    asarray([2, -2]),
+    asarray([0.25, 0.625]),
+    asarray([5.5, 2]),
+    asarray([-12, -2]),
+    asarray([8, 0]),
 )
 
 plot_2D_power_basis_curve(points)
